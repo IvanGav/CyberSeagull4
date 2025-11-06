b3bcbc5578b843431b098e6728b5efd1997f6ea9
Moss <moss@mossx.net>
2025-11-05 22:23:01 -0800

    Server: Made it work again

--- src/server.h
+++ src/server.h
@@ -14,22 +14,24 @@ class servergull : public cgull::net::server_interface<char> {
 					cgull::net::message<char> m;
 					m.header = { GIVE_PLAYER_ID, message_header_sizes[GIVE_PLAYER_ID] };
 					m.body = { (U8)(num_players & 0xff), (U8)((num_players >> 8) & 0xff) };
-				this->MessageClient(client, m);
-				return true;
-			}
+					num_players++;
+					this->MessageClient(client, m);
+					return true;
+				}
 
-			void OnClientDisconnect(std::shared_ptr <cgull::net::connection<char>> client) override {
-				std::cout << "Client disconnected\n";
-			}
+				void OnClientDisconnect(std::shared_ptr <cgull::net::connection<char>> client) override {
+					std::cout << "Client disconnected\n";
+				}
 
-			void OnMessage(std::shared_ptr<cgull::net::connection<char>> client, cgull::net::message<char>& msg) override {
-				switch (msg.header.id) {
+				void OnMessage(std::shared_ptr<cgull::net::connection<char>> client, cgull::net::message<char>& msg) override {
+					std::cout << "srv: msg\n";
+					switch (msg.header.id) {
 					case PLAYER_CAT_FIRE: {
 						this->MessageAllClients(msg, client);
 					}
-					break;
+						break;
+					}
 				}
-			}
 
 
 };
