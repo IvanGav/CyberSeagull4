3498022c6f70179ece70f4688dbe572156cd8143
EathanT <eathantillman11@gmail.com>
2025-11-08 07:16:59 -0800

    pkealse

--- Server/include/cgullnet/net_connection.h
+++ Server/include/cgullnet/net_connection.h
@@ -44,6 +44,7 @@ namespace cgull {
                         {
                             if (!ec)
                             {
+                                m_connected = true;
                                 ReadHeader();
                             }
                             else
