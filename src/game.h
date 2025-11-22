467cb849b0cd911216a88cefcc2850bbe6ce97f0
Victoria Worthington <victoworthington@gmail.com>
2025-11-21 22:58:44 -0800

    refactor: found and removed unused code from game.h

--- src/game.h
+++ src/game.h
@@ -5,46 +5,6 @@
 #include <algorithm>
 #include "util.h"
 
-#define BASE_COOLDOWN 2.0 // seconds
-
-struct Lane {
-	std::vector<F64> dists;
-};
-
-struct Player {
-	std::vector<F64> cooldowns;
-	std::vector<Lane> projectiles;
-
-	// Advance the time by `dt`
-	void advanceTime(F64 dt) {
-		for (U32 i = 0; i < cooldowns.size(); i++) {
-			cooldowns[i] = std::max(0.0, cooldowns[i] - dt);
-			std::vector<F64>& balls = projectiles[i].dists;
-			for (U32 j = 0; j < balls.size(); j++) {
-				balls[j] += dt;
-			}
-		}
-	}
-	// Shoot a ball in the specified lane. If it was still on cooldown, return the cooldown (0.0 or less otherwise)
-	F64 shoot(U32 lane) {
-		if (cooldowns[lane] > 0.0) return cooldowns[lane];
-		cooldowns[lane] = BASE_COOLDOWN;
-		projectiles[lane].dists.push_back(0.0);
-		return -1.0;
-	}
-
-	// When receiving the enemy shot packet, use this
-	void forceShoot(U32 lane) {
-		projectiles[lane].dists.push_back(0.0);
-	}
-};
-
-struct Game {
-	Player players[2]; // players[0] is you; players[1] is the enemy
-	U32 lanes;
-	F64 playerDist;
-};
-
 // Given a lane and dist, return the model matrix.
 glm::mat4 positionOnArc(F64 timeFromLaunch, U32 lane, F64 distBetweenEnemyShips, F32 angle, F32 height = 0.45) {
 	return glm::translate(glm::vec3(sin(angle) * timeFromLaunch, (-height / distBetweenEnemyShips * (timeFromLaunch) * (timeFromLaunch - distBetweenEnemyShips)), cos(angle) * timeFromLaunch));
